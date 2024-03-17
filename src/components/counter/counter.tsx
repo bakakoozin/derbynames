d47634ddaabfaeafd5bb5aa6ae65e7107a0bd4c4
bakakoozin <cyrilpebre@gmail.com>
2024-03-17 17:01:04 +0100

    first commit on tuto

---
+++ src/components/counter/counter.tsx
@@ -0,0 +1,15 @@
+//import styles from './counter.module.css';
+
+import { useState } from "react"
+
+export function Counter() {
+    const [count, setCount] = useState(0)
+
+    const handleClick = () => {
+        setCount((count) => count + 1)
+    }
+
+    return (
+        <button onClick={handleClick}>count is {count}</button>
+    )
+}
