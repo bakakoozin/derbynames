c6f826d732a362553cd8fdf08add1316c424064d
bakakoozin <cyrilpebre@gmail.com>
2024-04-05 20:53:06 +0200

    ajout des routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import { Counter } from './components/counter/counter'
-import { Pokemon } from './components/pokemon/pokemon'
-import { useDebounce } from './hooks/debounce.hook'
-import { TicTacToe } from './components/tic-tac-toe/tic-tac-toe'
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Layout } from "./components/Layout";
+import { Poke } from "./components/poketest";
 
-function App() {
-  const [name, setName] = useState('pikachu')
-  const debouncedName = useDebounce<string>(name, 1000)
-  return (
-    <>
-      <h1>TUTO APP</h1>
-        <TicTacToe />
-      <div className="card">
-        <input 
-        type="texte" 
-        value={name} 
-        onChange={(e) => setName(e.target.value)} 
-        />
-        <Pokemon name={debouncedName}/>
-        <Counter />
-        <p className='text-600 mt-10'>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+export function App() {
+  return <Routes>
+    <Route path="/" element={<Layout />} >
+      <Route path="" element={<Home />} />
+      <Route path="test">
+        <Route path="" element={<p>test</p>} />
+        <Route path=":name" element={<Poke />} />
+      </Route>
+    </Route>
+  </Routes>
 }
-
-export default App
