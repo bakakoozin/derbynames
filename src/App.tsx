d4e9b4f676f3b24a0ed9e70263df345f3c114754
bakakoozin <cyrilpebre@gmail.com>
2024-07-16 21:44:57 +0200

    changement visuel page travaux

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Derbynames</h1>
+      <div className="flex flex-col items-center">
+        <img className='size-32' src='/derbynames_logo_grey.png'></img>
+      </div>
       <div className="card">
         <img className='size-96' src='/carotte.png'></img>
         <p className='text-600 mt-10 text-xl'>
