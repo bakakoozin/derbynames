5563960fc1ad2d2ac49073049d2a59e573bfe0e8
bakakoozin <cyrilpebre@gmail.com>
2024-12-10 21:09:56 +0100

    on sait pas trop

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { Routes, Route } from "react-router-dom"
-import { Layout } from "./components/Layout"
-import { Derbynames } from "./pages/derbynames"
-import { Clubs } from "./pages/clubs"
-import { NotFound } from "./pages/not-found"
-import { ValidateEmail } from "./pages/validate-email"
-import { Legal } from "./pages/legal"
+import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Derbynames } from "./pages/derbynames";
+import { Clubs } from "./pages/clubs";
+import { NotFound } from "./pages/not-found";
+import { ValidateEmail } from "./pages/validate-email";
+import { Legal } from "./pages/legal";
 
 export function App() {
-  return <Routes>
-    <Route path="/" element={<Layout />} >
-      <Route path="" element={<Derbynames />} />
-      <Route path="/derbynames" element={<Derbynames />} />
-      <Route path="/clubs" element={<Clubs />} />
-      <Route path="/validate/:token" element={<ValidateEmail />} />
-      <Route path="/legal" element={<Legal />} />
-      <Route path='*' element={<NotFound />} />
-    </Route>
-  </Routes>
-
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Derbynames />} />
+        <Route path="/derbynames" element={<Derbynames />} />
+        <Route path="/clubs" element={<Clubs />} />
+        <Route path="/validate/:token" element={<ValidateEmail />} />
+        <Route path="/legal" element={<Legal />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 }
