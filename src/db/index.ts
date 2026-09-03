80b4716edacfafb296278d8149054c9635f2025c
bakakoozin <cyrilpebre@gmail.com>
2026-09-04 00:15:59 +0200

    test correctif bug affichage liste derbynames

--- src/db/index.ts
+++ src/db/index.ts
@@ -13,6 +13,8 @@ function getConnectionPool() {
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
+      enableKeepAlive: true,
+      keepAliveInitialDelay: 10000,
     });
   }
   return connectionPool;
